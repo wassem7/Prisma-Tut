40bfc1b599bdb98c9efbaa1f145f2a5e5d1cbdb1
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-04-06 11:15:32 +0100

    types of relationships in prisma

--- script.ts
+++ script.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  //   const user = await prisma.user.create({
+  //     data: { name: 'Widdad Yahya' },
+  //   });
+  const users = await prisma.user.findMany();
+  console.log(users);
+}
 
 main()
   .catch((error) => {
